72b1973e3dd5188c5525e89d4fc2c573cd97ca96
Rich McRary <rgmcrary@gmail.com>
2017-11-05 14:55:04 -0500

    Updated routes

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-//  Scrape and Display
-
-// Good luck!
 
 // Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
-
-// Requiring our Note and Article models
-// var Note = require('./models/Note.js');
-// var Article = require('./models/Article.js');
-// Our scraping tools
+var Article = require('./models/article');
 var request = require('request');
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
@@ -22,7 +15,7 @@ var app = express();
 var PORT = process.env.PORT || 3001;
 
 // Use body parser with our app
-//app.use()
+
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -51,75 +44,16 @@ db.once('open', function() {
 // ======
 
 // Main route
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build/index.html'));
+// });
 
-// View Saved Articles
-app.get('/saved', function(req, res) {
-  var hbsObj = {};
-  Article.find({ saved: true }, function(error, doc) {
-    if (error) {
-      res.json(error);
-    } else {
-      hbsObj.articles = doc;
-      res.render('saved', hbsObj);
-    }
-  });
-});
-
-// Creates a new note or replaces an existing note
-app.post('/api/articles/note/:id', function(req, res) {
-  var noteObject = {};
-  noteObject.post_id = req.params.id;
-  noteObject.noteText = req.body.note;
-  var newNote = new Note(noteObject);
-
-  newNote.save(function(error, doc) {
-    if (error) {
-      console.error(error);
-    } else {
-      res.json(doc);
-    }
-  });
-});
-
-// This will grab all notes by article Id
-app.get('/api/notes/:id', function(req, res) {
-  Note.find(
-    {
-      post_id: req.params.id
-    },
-    function(error, doc) {
-      if (error) {
-        res.json(error);
-      } else {
-        res.json(doc);
-      }
-    }
-  );
-});
-
-// Delete a note
-app.put('/api/articles/note/:id', function(req, res) {
-  var noteId = req.params.id;
-
-  Note.remove({ _id: noteId }, function(error, doc) {
-    if (error) {
-      console.error(error);
-    } else {
-      res.json(doc);
-    }
-  });
-});
 
 // Save an Article
 app.post('/api/articles/:id', function(req, res) {
-  var booleanVal = req.body.saved === 'true' ? true : false;
-  console.log(booleanVal);
   Article.findOneAndUpdate(
     { _id: req.params.id },
-    { $set: { saved: booleanVal } },
+    { $set: { saved: true } },
     function(error, doc) {
       if (error) {
         console.error(error);
@@ -129,6 +63,33 @@ app.post('/api/articles/:id', function(req, res) {
     }
   );
 });
+
+// View Saved Articles
+app.get('/api/articles', function(req, res) {
+  var hbsObj = {};
+  Article.find({ saved: true }, function(error, doc) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+// Delete a saved article
+app.put('/api/articles/:id', function(req, res) {
+  var articleId = req.params.id;
+  Article.remove({ _id: articleId }, function(error, doc) {
+    if (error) {
+      console.error(error);
+    } else {
+      res.json(doc);
+    }
+  });
+});
+
+
+
 
 // Listen on port 3000
 app.listen(PORT, function() {
