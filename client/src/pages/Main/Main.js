e5bc68b88d80677e98f785b416c22492fb47528d
Rich McRary <rgmcrary@gmail.com>
2017-11-06 17:04:47 -0500

    removed imports and unused code

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,34 +1,19 @@
-import React, { Component } from "react";
-import { Container } from "../../components/Grid";
-import Search from "../Search";
+import React, { Component } from 'react';
+import { Container } from '../../components/Grid';
+import Search from '../Search';
 import Saved from '../Saved';
-import API from '../../utils/API';
-
 
 class Main extends Component {
-  state = {
-    savedArticles: []
-  };
-
-  componentDidMount() {
-    this.loadSavedArticles();
-  } 
-
-  loadSavedArticles = () => {
-    API.getSavedArticles()
-      .then(res =>
-        this.setState({
-          savedArticles: res.data
-        })
-      )
-      .catch(err => console.log(err));
-  };
-
   render() {
-    return <Container fluid>
+    return (
+      <Container fluid>
         <Search loadSaved={this.loadSavedArticles} />
-        <Saved loadSaved={this.loadSavedArticles} articles={this.state.savedArticles} />
-      </Container>;
+        <Saved
+          loadSaved={this.loadSavedArticles}
+          articles={this.state.savedArticles}
+        />
+      </Container>
+    );
   }
 }
 
