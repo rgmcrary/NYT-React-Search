26d985cd2d3301a2015cc267ed6c6239c9412fb1
Rich McRary <rgmcrary@gmail.com>
2017-11-06 17:03:02 -0500

    removed unused code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,20 +1,7 @@
-const router = require("express").Router();
-const articleRoutes = require("./articles");
+const router = require('express').Router();
+const articleRoutes = require('./articles');
 
 // Article routes
-router.use("/articles", articleRoutes);
-
-// ReactDOM.render(
-//   <Router>
-//     <Route path="/" component={MainLayout}>
-//       <IndexRoute component={Home} />
-//       <Route component={SearchLayout}>
-//         <Route path="users" component={UserList} />
-//         <Route path="widgets" component={WidgetList} />
-//       </Route>
-//     </Route>
-//   </Router>,
-//   document.getElementById('root')
-// );
+router.use('/articles', articleRoutes);
 
 module.exports = router;
