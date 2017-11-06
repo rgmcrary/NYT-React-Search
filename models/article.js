3499c5b60ac6ff2ea16e0f58384f3d65cf7b14e5
Rich McRary <rgmcrary@gmail.com>
2017-11-06 13:23:18 -0500

    removed date field from Schema

--- models/article.js
+++ models/article.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
   title: { type: String, required: true },
-  date: { type: Date, default: Date.now },
   url: { type: String, required: true}
 });
 
