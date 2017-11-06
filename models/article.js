26b5fe4f3272b12f7967a3b3ff8716bc772d6f96
Rich McRary <rgmcrary@gmail.com>
2017-11-06 15:12:35 -0500

    added articleId property

--- models/article.js
+++ models/article.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
   title: { type: String, required: true },
-  url: { type: String, required: true}
+  url: { type: String, required: true },
+  articleId: { type: String }
 });
 
-const Article = mongoose.model("Article", articleSchema);
+const Article = mongoose.model('Article', articleSchema);
 
 module.exports = Article;
