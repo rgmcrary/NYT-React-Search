0fa480919aaab6ac03faf462cc3f20db82d20a4b
Rich McRary <rgmcrary@gmail.com>
2017-11-06 17:04:21 -0500

    removed Main import and added api code

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -2,21 +2,36 @@ import React, { Component } from 'react';
 import { List, ListItem } from '../../components/List';
 import { Col, Row } from '../../components/Grid';
 import { FormBtn } from '../../components/Form';
-import Main from '../Main';
 import API from '../../utils/API';
 
 class Saved extends Component {
+  state = {
+    savedArticles: []
+  };
+
+  componentDidMount() {
+    this.loadSavedArticles();
+  }
+
+  loadSavedArticles = () => {
+    API.getSavedArticles()
+      .then(res =>
+        this.setState({
+          savedArticles: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
 
   // Deletes an article from the database with a given id, then reloads articles from the db
   deleteArticle = id => {
     API.deleteArticle(id)
-      .then(res => this.props.loadSaved())
+      .then(res => this.loadSavedArticles())
       .catch(err => console.log(err));
   };
 
   render() {
-    return (
-      // This row will handle all of the saved articles
+    return (// This row will handle all of the saved articles
       <Row>
         <Col size="sm-12">
           <br />
@@ -33,38 +48,27 @@ class Saved extends Component {
             </div>
 
             {/* This main panel will hold each of the resulting articles */}
-            {this.props.articles.length ? (
-              <List>
-                {this.props.articles.map(article => {
-                  return (
-                    <ListItem key={article._id} style={{ height: '67px' }}>
+            {this.state.savedArticles.length ? <List>
+                {this.state.savedArticles.map(article => {
+                  return <ListItem key={article._id} style={{ height: '67px' }}>
                       <a href={article.url} target="_blank">
                         <strong>{article.title}</strong>
                       </a>
-                      <FormBtn
-                        onClick={() => this.deleteArticle(article._id)}
-                        style={{
-                          padding: 0,
-                          position: 'absolute',
-                          right: '20px'
-                        }}
-                      >
+                      <FormBtn onClick={() => this.deleteArticle(article._id)} style={{ padding: 0, position: 'absolute', right: '20px' }}>
                         Delete
                       </FormBtn>
-                    </ListItem>
-                  );
+                    </ListItem>;
                 })}
-              </List>
-            ) : (
-              <h3 style={{ marginLeft: 15 }}>No Results to Display</h3>
-            )}
+              </List> : <h3 style={{ marginLeft: 15 }}>
+                No Results to Display
+              </h3>}
 
             {/* This main panel will hold each of the resulting articles */}
             <div className="panel-body" id="well-section" />
           </div>
         </Col>
       </Row>
-    );
+    )
   }
 }
 
