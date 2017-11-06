3edb616a24ec5599617f57e36d95337a44a5fe9f
Rich McRary <rgmcrary@gmail.com>
2017-11-06 13:23:52 -0500

    Copied and re-factored new api file

---
+++ routes/api/articles.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const articlesController = require("../../controllers/articlesController");
+
+
+router
+  .route('/')
+  .get(articlesController.findAll)
+  .post(articlesController.create);
+
+router
+  .route("/:id")
+  .delete(articlesController.remove);
+
+module.exports = router;
+
+
